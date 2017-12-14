25aa3f578ec7b5dc65b28058a3efc703238e721f
Lara <larisa.sabalin@gmail.com>
2017-12-13 21:21:29 -0500

    created the about, events, 404 pages. finalized the index page.

--- scripts/larisa.js
+++ scripts/larisa.js
@@ -21,3 +21,33 @@
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var y=
+  document.getElementsByClassName("caption");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+     y[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+  y[slideIndex-1].style.display = "block";
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
